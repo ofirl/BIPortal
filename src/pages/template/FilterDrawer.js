766637b514a554fcff67e6f9ed11f53bc240aaae
Ofir Levi <ofirl24@gmail.com>
2019-08-22 21:17:42 +0300

    layour changes

--- src/pages/template/FilterDrawer.js
+++ src/pages/template/FilterDrawer.js
@@ -6,54 +6,152 @@ import Draggable from 'react-draggable';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Select from '@material-ui/core/Select';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles(theme => ({
+    drawer: {
+        // width: '30%'
+    },
+    drawerRoot: {
+        height: '100%',
+        // width:'30%'
+    },
+    drawerPaper: {
+        width: '100%',
+        position: 'relative',
+    },
     maxWidth100: {
-      maxWidth: '100%'
+        maxWidth: '100%'
     },
     filterChip: {
-      margin: '1px',
-    //   width: '50%',
-      direction: 'ltr'
+        margin: '1px',
+        //   width: '50%',
+        direction: 'ltr'
+    },
+    filterInputLine: {
+        flexGrow: '1',
+        width: '100%',
+        maxHeight: '3em',
+        alignItems: 'center'
+    },
+    filterInputLineLabel: {
+        flexGrow: '1',
+        width: '100%'
+    },
+    filterInputLineField: {
+        flexGrow: '1',
+        width: '100%'
     }
-  }));
+}));
 
-  /**
-   * This function will infer the filter value from the data
-   * @param {*[]} data 
-   */
-const inferFilters = function(data) {
+/**
+ * This function will infer the filter value from the data
+ * @param {*[]} data 
+ */
+function inferOptions(data, key) {
     if (data.length === 0)
         return {};
-
+    
     let filters = {};
-    Object.keys(data[0]).forEach( (value) => filters[value] = new Set() );
-    Object.keys(filters).forEach( (value) => {
-        let allVals = data.reduce( (prev, curr) => prev.add(curr), new Set());
+    if (key == null)
+        Object.keys(data[0]).forEach((value) => filters[value] = new Set());
+    else
+        filters[key] = new Set();
+
+    Object.keys(filters).forEach((value) => {
+        let allVals = data.reduce((prev, curr) => prev.add(curr[value]), new Set());
         filters[value] = allVals;
-    } );
+    });
+
+    return filters;
 }
 
+function getStyles(name, value, theme) {
+    return {
+        fontWeight:
+            value!= null && value.indexOf(name) === -1
+                ? theme.typography.fontWeightRegular
+                : theme.typography.fontWeightMedium,
+    };
+}
+
+const FilterRow = (props) => {
+    const { classes, filter, value, theme, options, handleChange } = props;
+
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: '20em',
+                // width: 200,
+            },
+        },
+    };
+    
+    if (filter.type === "list") {
+        return (
+            <FormControl className={classes.formControl}>
+                <InputLabel htmlFor="select-multiple-chip">כלי</InputLabel>
+                <Select
+                    multiple
+                    // autoWidth
+                    value={value}
+                    onChange={(e) => {handleChange(e.target.value.map( (v) => { return {operator: '=', value: v}}))}}
+                    input={<Input id="select-multiple-chip" />}
+                    renderValue={selected => (
+                        <div className={classes.chips}>
+                            {selected.map(value => (
+                                <Chip key={value} label={value} className={classes.chip} />
+                            ))}
+                        </div>
+                    )}
+                    MenuProps={MenuProps}
+                >
+                    {options.map(option => (
+                        <MenuItem key={option} value={option} style={getStyles(option, value, theme)}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </Select>
+            </FormControl>
+        );
+    }
+    return (
+        <Grid item container className={classes.filterInputLine}>
+            <Grid item xs={6} className={classes.filterInputLineLabel}>
+                test1
+            </Grid>
+            <Grid item xs={6} className={classes.filterInputLineField}>
+                test2
+            </Grid>
+        </Grid>
+    );
+}
 
 export default (props) => {
+    const { data, open, filters, activeFilters, onFilterChange } = props;
     const classes = useStyles();
+    const theme = useTheme();
 
     return (
         <Drawer
             className={classes.drawer}
             variant="persistent"
             anchor="left"
-            open={props.open}
+            open={open}
             classes={{
                 paper: classes.drawerPaper,
+                root: classes.drawerRoot
             }}
         >
-            <Grid container direction="column" style={{ height: '100%', width: '10em', direction: 'rtl' }}>
-                <div style={{ height: '5em' }}></div>
+            <Grid container direction="column" style={{ height: '100%', direction: 'rtl' }}>
+                {/* <div style={{ height: '5em' }}></div> */}
                 <Grid id="filterGrid" item xs={1} style={{ flexGrow: '1' }} container direction="column" className={classes.maxWidth100}>
-                    <Grid item xs={1} style={{ flexGrow: '1' }} className={classes.maxWidth100} direction="column">
+                    {/* <Grid item xs={1} style={{ flexGrow: '1' }} className={classes.maxWidth100} direction="column">
                         <Typography variant="subtitle2" style={{ fontWeight: 'bold' }}>
                             מסננים זמינים
                         </Typography>
@@ -70,9 +168,14 @@ export default (props) => {
                         <Typography variant="subtitle2" style={{ fontWeight: 'bold' }}>
                             מסננים אקטיבים
                         </Typography>
-                    </Grid>
+                    </Grid> */}
+                    {
+                        Object.keys(filters).map((val) => <FilterRow classes={classes} filter={filters[val]} theme={theme} options={[...(inferOptions(data, val)[val])]}
+                            handleChange={(newFilters) => onFilterChange(val, newFilters)}
+                            value={activeFilters[val] ? activeFilters[val].reduce((prev, curr) => { prev.push(curr.value); return prev; }, []) : []} />)
+                    }
                 </Grid>
-                <div style={{ height: '5em' }}></div>
+                {/* <div style={{ height: '5em' }}></div> */}
             </Grid>
         </Drawer>
     );
--- src/pages/template/Template.js
+++ src/pages/template/Template.js
@@ -71,14 +71,30 @@ const useStyles = makeStyles(theme => ({
 
   },
   content: {
-    // flexGrow: '1',
+    flexGrow: '1',
+    // flexShrink: '1',
   },
   maxWidth100: {
     maxWidth: '100%'
   },
 }));
 
-let exampleFilter = {
+let exampleFilterDefinition = {
+  name: {
+    type: 'list',
+    listType: 'single'
+  },
+  // name2: {
+  //   type: 'list',
+  //   listType: 'single'
+  // },
+  // name3: {
+  //   type: 'list',
+  //   listType: 'single'
+  // }
+};
+
+let exampleActiveFilter = {
   name: [{
     operator: '=',
     value: 'Page A'
@@ -103,7 +119,7 @@ const operatorConv = {
   "!=": (val1, val2) => val1 !== val2
 }
 
-const filterData = function (filter, data) {
+function filterData(filter, data) {
   let applyFilter = function (dataKey, filterObj, data) {
     return filterObj.some((filterEntry) => operatorConv[filterEntry.operator](filterEntry.value, data[dataKey]))
   }
@@ -111,16 +127,24 @@ const filterData = function (filter, data) {
   return data.filter((dataVal) => Object.keys(filter).every((filterVal, index, arr) => applyFilter(filterVal, filter[filterVal], dataVal)));
 };
 
-const onFilterChange = function () {
+function onFilterChange(key, newFilters, oldFilters) {
+  let allFilterObj = { ...oldFilters };
 
+  if (newFilters.length === 0)
+    delete (allFilterObj[key]);
+  else
+    allFilterObj[key] = newFilters;
+
+  return allFilterObj;
 }
 
 export default (props) => {
   const classes = useStyles();
   const [open, setOpen] = React.useState(false);
+  const [activeFilters, setActiveFilters] = React.useState({});
 
-  let filteredData = filterData(exampleFilter, data);
-  let filteredData2 = filterData(exampleFilter, data2);
+  let filteredData = filterData(activeFilters, data);
+  let filteredData2 = filterData(activeFilters, data2);
 
   return (
     <React.Fragment>
@@ -128,67 +152,58 @@ export default (props) => {
 
       <Grid container spacing={0} xs direction="column" className={classes.mainContainer}>
         <TopBar toggleFilterOpen={() => setOpen(!open)} />
-        <Grid item xs={8} className={[classes.maxWidth100, classes.content]}>
-          <Box display="flex" style={{ height: '100%' }}>
-            {/* content
-              b <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            a <br />
-            b <br /> */}
-            <Paper style={{ width: '50%', padding: '20px', margin: '10px' }}>
-              <ResponsiveContainer height={500} width={"100%"}>
-                <LineChart data={filteredData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
-                  <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                </LineChart>
-              </ResponsiveContainer>
-            </Paper>
+        <Grid item container xs={1} style={{ flexGrow: '1', maxWidth: '100%', height: '100%', width: '100%' }}>
+          <Grid item xs={open ? 0 : 0} style={{flexGrow: open ? 0.3 : 0, maxWidth: '100%', transition: open ? 'flex-grow 225ms cubic-bezier(0, 0, 0.2, 1) 0ms': '' }}>
+            <FilterDrawer open={open} data={data} filters={exampleFilterDefinition} activeFilters={activeFilters}
+              onFilterChange={(key, newFilter) => setActiveFilters(onFilterChange(key, newFilter, activeFilters))} />
+          </Grid>
+          <Grid item xs={1} className={[classes.maxWidth100, classes.content]} container>
+            {/* <Grid item xs={1} style={{ flexGrow: '1', height:'100%', maxWidth:'100%' }}> */}
+            <Box display="flex" style={{ height: '100%', width: '100%' }}>
+              <Grid item xs={1} style={{ flexGrow: '1', maxWidth: '100%', height: '100%', width: '100%' }}>
+                <Paper style={{ padding: '20px', margin: '10px' }}>
+                  <ResponsiveContainer height={500} width={"100%"}>
+                    <LineChart data={filteredData} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <Tooltip />
+                    </LineChart>
+                  </ResponsiveContainer>
+                </Paper>
+              </Grid>
+              <Grid item xs={1} style={{ flexGrow: '1', maxWidth: '100%', height: '100%', width: '100%' }}>
+                <Paper style={{ width: '50%', padding: '20px', margin: '10px', height: '50%' }}>
+                  <ResponsiveContainer height={"100%"} width={"100%"}>
+                    <ComposedChart data={filteredData2}>
+                      <XAxis dataKey="name" />
+                      <YAxis />
+                      <Tooltip />
+                      <Legend />
+                      <CartesianGrid stroke="#f5f5f5" />
+                      <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
+                      <Bar dataKey="pv" barSize={20} fill="#413ea0" />
+                      <Line type="monotone" dataKey="uv" stroke="#ff7300" />
+                    </ComposedChart>
+                  </ResponsiveContainer>
+                </Paper>
+              </Grid>
+            </Box>
+            {/* </Grid> */}
+            {/* <Grid item xs={1} style={{ flexGrow: '0.5', maxWidth: '100%' }}>
+            <FilterDrawer open={open} data={data} filters={exampleFilterDefinition} activeFilters={activeFilters}
+              onFilterChange={(key, newFilter) => setActiveFilters(onFilterChange(key, newFilter, activeFilters))} />
+          </Grid> */}
 
-            <Paper style={{ width: '50%', padding: '20px', margin: '10px', height:'50%' }}>
-              <ResponsiveContainer height={"100%"} width={"100%"}>
-                <ComposedChart data={filteredData2}>
-                  <XAxis dataKey="name" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend />
-                  <CartesianGrid stroke="#f5f5f5" />
-                  <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
-                  <Bar dataKey="pv" barSize={20} fill="#413ea0" />
-                  <Line type="monotone" dataKey="uv" stroke="#ff7300" />
-                </ComposedChart>
-              </ResponsiveContainer>
-            </Paper>
-          </Box>
-
+          </Grid>
         </Grid>
+
         {/* <BottomBar /> */}
       </Grid>
 
-      <FilterDrawer open={open} data={data} activeFilters={exampleFilter} onFilterChange={onFilterChange} />
+      {/* <FilterDrawer open={open} data={data} filters={exampleFilterDefinition} activeFilters={activeFilters}
+        onFilterChange={(key, newFilter) => setActiveFilters(onFilterChange(key, newFilter, activeFilters))} /> */}
 
     </React.Fragment>
   )
